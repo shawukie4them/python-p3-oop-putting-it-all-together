e215d4d40c83fba27e226321345fe8b9f08775dc
shawukie4them <shawukiekawuki@gmail.com>
2025-02-28 12:50:27 +0300

    Objected oriented programming.

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,45 @@
 #!/usr/bin/env python3
-
+import sys
 class Book:
-    pass
+    def __init__(self, title, page_count, author="Unknown", genre="Unknown"):
+        self.title = title
+        self.page_count = page_count
+        self.author = author
+        self.genre = genre
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 50:
+            self._title = value.title()
+        else:
+            raise ValueError("Title must be a string between 1 and 50 characters.")
+        
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 50:
+            self._author = value.title()
+        else:
+            raise ValueError("Author must be a string between 1 and 50 characters.")
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int) and value > 0:
+            self._page_count = value
+        else:
+            print("page_count must be an integer", file=sys.stdout)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!", file=sys.stdout)
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
-
+import sys
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    @property
+    def brand(self):
+        return self._brand
+    
+    @brand.setter
+    def brand(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._brand = value.title()
+        else:
+            raise ValueError("Brand must be a non-empty string.")
+        
+    @property
+    def size(self):
+        return self._size 
+    
+    @size.setter
+    def size(self, value):
+        if isinstance(value, (int, float)) and value > 0:
+            self._size = value
+        else:
+            print("size must be an integer", file=sys.stdout)
+    
+    def cobble(self):
+        print("Your shoe is as good as new!", file=sys.stdout)
+        self.condition = "New"
